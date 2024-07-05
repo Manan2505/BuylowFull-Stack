826c5b39b049d1bde0c4a2870617afb4dc668f4f
Manan Arora <arora.manan.k1234@gmail.com>
2024-07-05 15:08:40 +0530

    la

--- e-commerce-backend/config/database.js
+++ e-commerce-backend/config/database.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
  dbConnect=()=>{
     try{
-        mongoose.connect("mongodb+srv://buylow:buylow@cluster0.ekc7wew.mongodb.net/",{
+        mongoose.connect("mongodb+srv://buylow:buylow@cluster0.ekc7wew.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
             useNewUrlParser:true,
             useUnifiedTopology:true,
             family:4
